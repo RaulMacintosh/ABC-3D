2cced63f3a284169b6c038ac8ca232cccdf738b7
Raul Silveira <raul95@gmail.com>
2017-10-14 11:26:15 -0300

    Letters from keyboard to LED Cube

--- Display/Display.ino
+++ Display/Display.ino
@@ -6,26 +6,6 @@ byte cube[64];
 byte shifts[8];
 
 void setup() {
-
-  printU();
-
-//  cube[0] = 170;
-//  cube[8] = 24;
-//  cube[16] = 170;
-//  cube[24] = 24;
-//  cube[32] = 170;
-//  cube[40] = 24;
-//  cube[48] = 170;
-//  cube[56] = 24;
-//
-//  cube[1] = 170;
-//  cube[9] = 24;
-//  cube[17] = 170;
-//  cube[25] = 24;
-//  cube[33] = 170;
-//  cube[41] = 24;
-//  cube[49] = 170;
-//  cube[57] = 24;
   Serial.begin(9600);
 
   //Setting and clearing layers
@@ -43,66 +23,7 @@ void setup() {
   digitalWrite(CLOCK_PIN, LOW);
 }
 
-void printU(){
-  cube[0] = 3;
-  cube[1] = 3;
-  cube[2] = 3;
-  cube[3] = 3;
-  cube[4] = 3;
-  cube[5] = 3;
-  cube[6] = 3;
-  cube[7] = 3;
-  
-  cube[8] = 3;
-  cube[9] = 3;
-  cube[10] = 3;
-  cube[11] = 3;
-  cube[12] = 3;
-  cube[13] = 3;
-  cube[14] = 3;
-  cube[15] = 3;
-  
-  cube[16] = 3;
-  cube[17] = 3;
-  cube[22] = 3;
-  cube[23] = 3;
-  
-  cube[24] = 3;
-  cube[25] = 3;
-  cube[30] = 3;
-  cube[31] = 3;
-  
-  cube[32] = 3;
-  cube[33] = 3;
-  cube[38] = 3;
-  cube[39] = 3;
-  
-  cube[40] = 3;
-  cube[41] = 3;
-  cube[46] = 3;
-  cube[47] = 3;
-  
-  cube[48] = 3;
-  cube[49] = 3;
-  cube[54] = 3;
-  cube[55] = 3;
-  
-  cube[56] = 3;
-  cube[57] = 3;
-  cube[62] = 3;
-  cube[63] = 3;
-}
-
-//int count = 0;
-void loop() {
-
-//  cube[count/8] = 255;
-//
-//  count++;
-//  if(count == 512){
-//    count = 0;
-//  }
-
+void setCube(){
   for(int i = 0; i < 64; i += 8){
 
     for(int j = 0; j < 8; j++){
@@ -135,15 +56,10 @@ void loop() {
       shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, shifts[l]);
     }
     digitalWrite(LATCH_PIN, HIGH);
+  };
+}
 
-    //delay(500);
-    if(i > 0){
-      digitalWrite(((i-8)/8)+2, LOW);
-    }else{
-      digitalWrite((7)+2, LOW);
-    }
-    //delay(500);
-  }
+void loop() {
 }
 
 int bytesRead = 0;
@@ -151,6 +67,7 @@ void serialEvent() {
     cube[bytesRead] = (byte) Serial.read();
     if(bytesRead==63){
       bytesRead = 0;
+      setCube();
     }else{
       bytesRead++;
     }
--- Keyboard/Keyboard.ino
+++ Keyboard/Keyboard.ino
@@ -27,6 +27,8 @@
 
 #define ENTER 8
 
+byte cube[64];
+
 void setup() {
   pinMode(A, INPUT);
   pinMode(B, INPUT);
@@ -60,114 +62,783 @@ void setup() {
   Serial.begin(9600);
 }
 
+void clearCube(){
+  for(int i = 0; i < 64; i++){
+    cube[i] = 0b00000000;
+  }
+}
+
+void printA(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b00110011;
+  cube[51] = 0b00110011;
+  cube[52] = 0b00110011;
+  cube[53] = 0b00110011;
+  cube[54] = 0b11111111;
+  cube[55] = 0b11111111;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b00110011;
+  cube[59] = 0b00110011;
+  cube[60] = 0b00110011;
+  cube[61] = 0b00110011;
+  cube[62] = 0b11111111;
+  cube[63] = 0b11111111;
+}
+
+void printB(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b11011011;
+  cube[51] = 0b11011011;
+  cube[52] = 0b11011011;
+  cube[53] = 0b11011011;
+  cube[54] = 0b01111110;
+  cube[55] = 0b00111100;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b11011011;
+  cube[59] = 0b11011011;
+  cube[60] = 0b11011011;
+  cube[61] = 0b11011011;
+  cube[62] = 0b01111110;
+  cube[63] = 0b00111100;
+}
+
+void printC(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b11000011;
+  cube[51] = 0b11000011;
+  cube[52] = 0b11000011;
+  cube[53] = 0b11000011;
+  cube[54] = 0b11000011;
+  cube[55] = 0b11000011;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b11000011;
+  cube[59] = 0b11000011;
+  cube[60] = 0b11000011;
+  cube[61] = 0b11000011;
+  cube[62] = 0b11000011;
+  cube[63] = 0b11000011;
+}
+
+void printD(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b11000011;
+  cube[51] = 0b11000011;
+  cube[52] = 0b01000010;
+  cube[53] = 0b00100100;
+  cube[54] = 0b00011000;
+  cube[55] = 0b00011000;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b11000011;
+  cube[59] = 0b11000011;
+  cube[60] = 0b01000010;
+  cube[61] = 0b00100100;
+  cube[62] = 0b00011000;
+  cube[63] = 0b00011000;
+}
+
+void printE(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b11011011;
+  cube[51] = 0b11011011;
+  cube[52] = 0b11011011;
+  cube[53] = 0b11011011;
+  cube[54] = 0b11011011;
+  cube[55] = 0b11011011;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b11011011;
+  cube[59] = 0b11011011;
+  cube[60] = 0b11011011;
+  cube[61] = 0b11011011;
+  cube[62] = 0b11011011;
+  cube[63] = 0b11011011;
+}
+
+void printF(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b00011011;
+  cube[51] = 0b00011011;
+  cube[52] = 0b00011011;
+  cube[53] = 0b00011011;
+  cube[54] = 0b00011011;
+  cube[55] = 0b00011011;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b00011011;
+  cube[59] = 0b00011011;
+  cube[60] = 0b00011011;
+  cube[61] = 0b00011011;
+  cube[62] = 0b00011011;
+  cube[63] = 0b00011011;
+}
+
+void printG(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b11000011;
+  cube[51] = 0b11000011;
+  cube[52] = 0b11011011;
+  cube[53] = 0b11011011;
+  cube[54] = 0b11111011;
+  cube[55] = 0b11111011;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b11000011;
+  cube[59] = 0b11000011;
+  cube[60] = 0b11011011;
+  cube[61] = 0b11011011;
+  cube[62] = 0b11111011;
+  cube[63] = 0b11111011;
+}
+
+void printH(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b00011000;
+  cube[51] = 0b00011000;
+  cube[52] = 0b00011000;
+  cube[53] = 0b00011000;
+  cube[54] = 0b11111111;
+  cube[55] = 0b11111111;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b00011000;
+  cube[59] = 0b00011000;
+  cube[60] = 0b00011000;
+  cube[61] = 0b00011000;
+  cube[62] = 0b11111111;
+  cube[63] = 0b11111111;
+}
+
+void printI(){
+  cube[48] = 0b00000000;
+  cube[49] = 0b00000000;
+  cube[50] = 0b00000000;
+  cube[51] = 0b11111111;
+  cube[52] = 0b11111111;
+  cube[53] = 0b00000000;
+  cube[54] = 0b00000000;
+  cube[55] = 0b00000000;
+  
+  cube[56] = 0b00000000;
+  cube[57] = 0b00000000;
+  cube[58] = 0b00000000;
+  cube[59] = 0b11111111;
+  cube[60] = 0b11111111;
+  cube[61] = 0b00000000;
+  cube[62] = 0b00000000;
+  cube[63] = 0b00000000;
+}
+
+void printJ(){
+  cube[48] = 0b11110000;
+  cube[49] = 0b11110000;
+  cube[50] = 0b11000000;
+  cube[51] = 0b11000000;
+  cube[52] = 0b11000000;
+  cube[53] = 0b11000000;
+  cube[54] = 0b11111111;
+  cube[55] = 0b11111111;
+  
+  cube[56] = 0b11110000;
+  cube[57] = 0b11110000;
+  cube[58] = 0b11000000;
+  cube[59] = 0b11000000;
+  cube[60] = 0b11000000;
+  cube[61] = 0b11000000;
+  cube[62] = 0b11111111;
+  cube[63] = 0b11111111;
+}
+
+void printK(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b00011000;
+  cube[51] = 0b00111100;
+  cube[52] = 0b01100110;
+  cube[53] = 0b11000011;
+  cube[54] = 0b10000001;
+  cube[55] = 0b00000000;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b00011000;
+  cube[59] = 0b00111100;
+  cube[60] = 0b01100110;
+  cube[61] = 0b11000011;
+  cube[62] = 0b10000001;
+  cube[63] = 0b00000000;
+}
+
+void printL(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b11000000;
+  cube[51] = 0b11000000;
+  cube[52] = 0b11000000;
+  cube[53] = 0b11000000;
+  cube[54] = 0b11000000;
+  cube[55] = 0b11000000;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b11000000;
+  cube[59] = 0b11000000;
+  cube[60] = 0b11000000;
+  cube[61] = 0b11000000;
+  cube[62] = 0b11000000;
+  cube[63] = 0b11000000;
+}
+
+void printM(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b00000110;
+  cube[51] = 0b00011000;
+  cube[52] = 0b00011000;
+  cube[53] = 0b00000110;
+  cube[54] = 0b11111111;
+  cube[55] = 0b11111111;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b00000110;
+  cube[59] = 0b00011000;
+  cube[60] = 0b00011000;
+  cube[61] = 0b00000110;
+  cube[62] = 0b11111111;
+  cube[63] = 0b11111111;
+}
+
+void printN(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b00000011;
+  cube[51] = 0b00001100;
+  cube[52] = 0b00110000;
+  cube[53] = 0b11000000;
+  cube[54] = 0b11111111;
+  cube[55] = 0b11111111;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b00000011;
+  cube[59] = 0b00001100;
+  cube[60] = 0b00110000;
+  cube[61] = 0b11000000;
+  cube[62] = 0b11111111;
+  cube[63] = 0b11111111;
+}
+
+void printO(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b11000011;
+  cube[51] = 0b11000011;
+  cube[52] = 0b11000011;
+  cube[53] = 0b11000011;
+  cube[54] = 0b11111111;
+  cube[55] = 0b11111111;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b11000011;
+  cube[59] = 0b11000011;
+  cube[60] = 0b11000011;
+  cube[61] = 0b11000011;
+  cube[62] = 0b11111111;
+  cube[63] = 0b11111111;
+}
+
+void printP(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b00110011;
+  cube[51] = 0b00110011;
+  cube[52] = 0b00110011;
+  cube[53] = 0b00110011;
+  cube[54] = 0b00111111;
+  cube[55] = 0b00111111;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b00110011;
+  cube[59] = 0b00110011;
+  cube[60] = 0b00110011;
+  cube[61] = 0b00110011;
+  cube[62] = 0b00111111;
+  cube[63] = 0b00111111;
+}
+
+void printQ(){
+  cube[48] = 0b00111111;
+  cube[49] = 0b00111111;
+  cube[50] = 0b00110011;
+  cube[51] = 0b00110011;
+  cube[52] = 0b00110011;
+  cube[53] = 0b00110011;
+  cube[54] = 0b11111111;
+  cube[55] = 0b11111111;
+  
+  cube[56] = 0b00111111;
+  cube[57] = 0b00111111;
+  cube[58] = 0b00110011;
+  cube[59] = 0b00110011;
+  cube[60] = 0b00110011;
+  cube[61] = 0b00110011;
+  cube[62] = 0b11111111;
+  cube[63] = 0b11111111;
+}
+
+void printR(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b00110011;
+  cube[51] = 0b00110011;
+  cube[52] = 0b00110011;
+  cube[53] = 0b01110011;
+  cube[54] = 0b10111111;
+  cube[55] = 0b00111111;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b00110011;
+  cube[59] = 0b00110011;
+  cube[60] = 0b00110011;
+  cube[61] = 0b01110011;
+  cube[62] = 0b10111111;
+  cube[63] = 0b00111111;
+}
+
+void printS(){
+  cube[48] = 0b11011111;
+  cube[49] = 0b11011111;
+  cube[50] = 0b11011011;
+  cube[51] = 0b11011011;
+  cube[52] = 0b11011011;
+  cube[53] = 0b11011011;
+  cube[54] = 0b11111011;
+  cube[55] = 0b11111011;
+  
+  cube[56] = 0b11011111;
+  cube[57] = 0b11011111;
+  cube[58] = 0b11011011;
+  cube[59] = 0b11011011;
+  cube[60] = 0b11011011;
+  cube[61] = 0b11011011;
+  cube[62] = 0b11111011;
+  cube[63] = 0b11111011;
+}
+
+void printT(){
+  cube[48] = 0b00000011;
+  cube[49] = 0b00000011;
+  cube[50] = 0b00000011;
+  cube[51] = 0b11111111;
+  cube[52] = 0b11111111;
+  cube[53] = 0b00000011;
+  cube[54] = 0b00000011;
+  cube[55] = 0b00000011;
+  
+  cube[56] = 0b00000011;
+  cube[57] = 0b00000011;
+  cube[58] = 0b00000011;
+  cube[59] = 0b11111111;
+  cube[60] = 0b11111111;
+  cube[61] = 0b00000011;
+  cube[62] = 0b00000011;
+  cube[63] = 0b00000011;
+}
+
+void printU(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b11000000;
+  cube[51] = 0b11000000;
+  cube[52] = 0b11000000;
+  cube[53] = 0b11000000;
+  cube[54] = 0b11111111;
+  cube[55] = 0b11111111;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b11000000;
+  cube[59] = 0b11000000;
+  cube[60] = 0b11000000;
+  cube[61] = 0b11000000;
+  cube[62] = 0b11111111;
+  cube[63] = 0b11111111;
+}
+
+void printV(){
+  cube[48] = 0b00000011;
+  cube[49] = 0b00001100;
+  cube[50] = 0b00110000;
+  cube[51] = 0b11000000;
+  cube[52] = 0b11000000;
+  cube[53] = 0b00110000;
+  cube[54] = 0b00001100;
+  cube[55] = 0b00000011;
+  
+  cube[56] = 0b00000011;
+  cube[57] = 0b00001100;
+  cube[58] = 0b00110000;
+  cube[59] = 0b11000000;
+  cube[60] = 0b11000000;
+  cube[61] = 0b00110000;
+  cube[62] = 0b00001100;
+  cube[63] = 0b00000011;
+}
+
+void printW(){
+  cube[48] = 0b11111111;
+  cube[49] = 0b11111111;
+  cube[50] = 0b01100000;
+  cube[51] = 0b00011000;
+  cube[52] = 0b00011000;
+  cube[53] = 0b01100000;
+  cube[54] = 0b11111111;
+  cube[55] = 0b11111111;
+  
+  cube[56] = 0b11111111;
+  cube[57] = 0b11111111;
+  cube[58] = 0b01100000;
+  cube[59] = 0b00011000;
+  cube[60] = 0b00011000;
+  cube[61] = 0b01100000;
+  cube[62] = 0b11111111;
+  cube[63] = 0b11111111;
+}
+
+void printX(){
+  cube[48] = 0b11000011;
+  cube[49] = 0b11000011;
+  cube[50] = 0b01100110;
+  cube[51] = 0b00111100;
+  cube[52] = 0b00111100;
+  cube[53] = 0b01100110;
+  cube[54] = 0b11000011;
+  cube[55] = 0b11000011;
+  
+  cube[56] = 0b11000011;
+  cube[57] = 0b11000011;
+  cube[58] = 0b01100110;
+  cube[59] = 0b00111000;
+  cube[60] = 0b00111000;
+  cube[61] = 0b01100110;
+  cube[62] = 0b11000011;
+  cube[63] = 0b11000011;
+}
+
+void printY(){
+  cube[48] = 0b00000011;
+  cube[49] = 0b00000110;
+  cube[50] = 0b00001100;
+  cube[51] = 0b11111000;
+  cube[52] = 0b11111000;
+  cube[53] = 0b00001100;
+  cube[54] = 0b00000110;
+  cube[55] = 0b00000011;
+  
+  cube[56] = 0b00000011;
+  cube[57] = 0b00000110;
+  cube[58] = 0b00001100;
+  cube[59] = 0b11111000;
+  cube[60] = 0b11111000;
+  cube[61] = 0b00001100;
+  cube[62] = 0b00000110;
+  cube[63] = 0b00000011;
+}
+
+void printZ(){
+  cube[48] = 0b11000011;
+  cube[49] = 0b11000111;
+  cube[50] = 0b11001111;
+  cube[51] = 0b11011011;
+  cube[52] = 0b11011011;
+  cube[53] = 0b11110011;
+  cube[54] = 0b11100011;
+  cube[55] = 0b11000011;
+  
+  cube[56] = 0b11000011;
+  cube[57] = 0b11000111;
+  cube[58] = 0b11001111;
+  cube[59] = 0b11011011;
+  cube[60] = 0b11011011;
+  cube[61] = 0b11110011;
+  cube[62] = 0b11100011;
+  cube[63] = 0b11000011;
+}
+
 void loop() {
   if(digitalRead(ENTER) == 1){
-    Serial.println("");
+//    clearCube();
+//    printE();
+//    for(int i = 0; i < 64; i++){
+//      Serial.write(cube[i]);
+//    }
+//    delay(1000);
+//
+//    clearCube();
+//    printL();
+//    for(int i = 0; i < 64; i++){
+//      Serial.write(cube[i]);
+//    }
+//    delay(1000);
+//
+//    clearCube();
+//    printO();
+//    for(int i = 0; i < 64; i++){
+//      Serial.write(cube[i]);
+//    }
+//    delay(1000);
+//
+//    clearCube();
+//    printI();
+//    for(int i = 0; i < 64; i++){
+//      Serial.write(cube[i]);
+//    }
+//    delay(1000);
+//
+//    clearCube();
+//    printS();
+//    for(int i = 0; i < 64; i++){
+//      Serial.write(cube[i]);
+//    }
+//    delay(1000);
+//
+//    clearCube();
+//    printA();
+//    for(int i = 0; i < 64; i++){
+//      Serial.write(cube[i]);
+//    }
     delay(500);
   }
 
   if(digitalRead(A) == 1){
-    Serial.print("A");
+    clearCube();
+    printA();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(B) == 1){
-    Serial.print("B");
+    clearCube();
+    printB();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(C) == 1){
-    Serial.print("C");
+    clearCube();
+    printC();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(D) == 1){
-    Serial.print("D");
+    clearCube();
+    printD();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(E) == 1){
-    Serial.print("E");
+    clearCube();
+    printE();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(F) == 1){
-    Serial.print("F");
+    clearCube();
+    printF();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(G) == 1){
-    Serial.print("G");
+    clearCube();
+    printG();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(H) == 1){
-    Serial.print("H");
+    clearCube();
+    printH();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(I) == 1){
-    Serial.print("I");
+    clearCube();
+    printI();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(J) == 1){
-    Serial.print("J");
+    clearCube();
+    printJ();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(K) == 1){
-    Serial.print("K");
+    clearCube();
+    printK();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(L) == 1){
-    Serial.print("L");
+    clearCube();
+    printL();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(M) == 1){
-    Serial.print("M");
+    clearCube();
+    printM();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(N) == 1){
-    Serial.print("N");
+    clearCube();
+    printN();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(O) == 1){
-    Serial.print("O");
+    clearCube();
+    printO();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(P) == 1){
-    Serial.print("P");
+    clearCube();
+    printP();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(Q) == 1){
-    Serial.print("Q");
+    clearCube();
+    printQ();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(R) == 1){
-    Serial.print("R");
+    clearCube();
+    printR();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(S) == 1){
-    Serial.print("S");
+    clearCube();
+    printS();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(T) == 1){
-    Serial.print("T");
+    clearCube();
+    printT();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(U) == 1){
-    Serial.print("U");
+    clearCube();
+    printU();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(V) == 1){
-    Serial.print("V");
+    clearCube();
+    printV();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(W) == 1){
-    Serial.print("W");
+    clearCube();
+    printW();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(X) == 1){
-    Serial.print("X");
+    clearCube();
+    printX();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(Y) == 1){
-    Serial.print("Y");
+    clearCube();
+    printY();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
   if(digitalRead(Z) == 1){
-    Serial.print("Z");
+    clearCube();
+    printZ();
+    for(int i = 0; i < 64; i++){
+      Serial.write(cube[i]);
+    }
     delay(500);
   }
 }
